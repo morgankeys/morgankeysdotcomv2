dbcb908420fc527ed63d4bdff6ff3834181ab2e8
Morgan Keys <morgan.keys@gmail.com>
2018-07-01 17:23:58 -0700

    Starting portfolio v2 as a React app. Mader a splash page to play with fonts.

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="splash">
+          <div className="headline">
+            <h1>Hi, I'm Morgan.</h1>
+            <p>
+              I’m a product designer who loves decision science, design
+              technologies, and people. Most of all, people. Take a look around
+              and then let’s chat.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
