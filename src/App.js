7d7fced1a653ce9b84e703567596875f493c23fc
Morgan Keys <morgan.keys@gmail.com>
2018-07-29 23:23:50 -0700

    Updated README

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import "./styles/home.css";
 import BulletPoints from "./assets/content/bulletPoints";
 import StoryPanel from "./components/StoryPanel/StoryPanel";
-// import splash from "./assets/images/splash.png";
 
 class Headline extends Component {
   constructor(props) {
@@ -18,17 +17,19 @@ class Headline extends Component {
     const classes = this.props.expanded ? "headline" : "headline collapsed";
     return (
       <div className={classes}>
-        <h1>Hi, I'm Morgan.</h1>
-        <p>
-          I’m a product designer who loves decision science, design
-          technologies, and people. Most of all, people. Take a look around and
-          then let’s chat.
-        </p>
-        <p>
-          <a className="cta-link" onClick={this.handleClick}>
-            Let's do it »
-          </a>
-        </p>
+        <div className="text-block">
+          <h1>Hi, I'm Morgan.</h1>
+          <p>
+            I’m a product designer who loves decision science, design
+            technologies, and people. Most of all, people. Take a look around
+            and then let’s chat.
+          </p>
+          <p>
+            <a className="cta-link" onClick={this.handleClick}>
+              Let's do it »
+            </a>
+          </p>
+        </div>
       </div>
     );
   }
@@ -88,16 +89,44 @@ class App extends Component {
 
     return (
       <div className={appClass}>
-        <div className="header">
+        {/* <div className="header">
           <Avatar onClick={this.reset}>
             <Headline onClickLetsGo={this.setCollapsed} />
           </Avatar>
           <Profile />
-        </div>
+        </div> */}
+        <CoverPage />
         {this.state.collapsed ? null : <StoryPanel />}
       </div>
     );
   }
 }
+
+const CoverPage = () => {
+  return (
+    <div className="cover-page">
+      <Avatar onClick={this.reset} />
+      <Headline onClickLetsGo={this.setCollapsed} />
+      <div className="name-and-bullets">
+        <h2>Morgan Keys</h2>
+        <div>
+          <a
+            className="subtle"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://twitter.com/@morgankeys"
+          >
+            @morgankeys
+          </a>
+        </div>
+        <div className="bullet-points">
+          {BulletPoints.map((bullet, index) => {
+            return <p key={index}>{bullet}</p>;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
