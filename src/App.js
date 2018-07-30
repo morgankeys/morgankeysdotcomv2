bc7fc96012d07e1a3ebf431e4f529b61d34e6480
Morgan Keys <morgan.keys@gmail.com>
2018-07-29 23:47:26 -0700

    Adventures in positioning

--- src/App.js
+++ src/App.js
@@ -3,37 +3,25 @@ import "./styles/home.css";
 import BulletPoints from "./assets/content/bulletPoints";
 import StoryPanel from "./components/StoryPanel/StoryPanel";
 
-class Headline extends Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(event) {
-    this.props.onClickLetsGo();
-  }
-
-  render() {
-    const classes = this.props.expanded ? "headline" : "headline collapsed";
-    return (
-      <div className={classes}>
-        <div className="text-block">
-          <h1>Hi, I'm Morgan.</h1>
-          <p>
-            I’m a product designer who loves decision science, design
-            technologies, and people. Most of all, people. Take a look around
-            and then let’s chat.
-          </p>
-          <p>
-            <a className="cta-link" onClick={this.handleClick}>
-              Let's do it »
-            </a>
-          </p>
-        </div>
+const Headline = props => {
+  return (
+    <div className="headline">
+      <div className="text-block">
+        <h1>Hi, I'm Morgan.</h1>
+        <p>
+          I’m a product designer who loves decision science, design
+          technologies, and people. Most of all, people. Take a look around and
+          then let’s chat.
+        </p>
+        <p>
+          <a className="cta-link" onClick={props.onClickLetsGo}>
+            Let's do it »
+          </a>
+        </p>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 const Avatar = props => {
   return (
@@ -43,24 +31,28 @@ const Avatar = props => {
   );
 };
 
-const Profile = () => {
+const CoverPage = props => {
   return (
-    <div className="profile">
-      <h2>Morgan Keys</h2>
-      <div>
-        <a
-          className="subtle"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://twitter.com/@morgankeys"
-        >
-          @morgankeys
-        </a>
-      </div>
-      <div className="bullet-points">
-        {BulletPoints.map((bullet, index) => {
-          return <p key={index}>{bullet}</p>;
-        })}
+    <div className="cover-page">
+      <Avatar onClick={props.reset} />
+      <Headline onClickLetsGo={props.setCollapsed} />
+      <div className="name-and-bullets">
+        <h2>Morgan Keys</h2>
+        <div>
+          <a
+            className="subtle"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://twitter.com/@morgankeys"
+          >
+            @morgankeys
+          </a>
+        </div>
+        <div className="bullet-points">
+          {BulletPoints.map((bullet, index) => {
+            return <p key={index}>{bullet}</p>;
+          })}
+        </div>
       </div>
     </div>
   );
@@ -89,44 +81,14 @@ class App extends Component {
 
     return (
       <div className={appClass}>
-        {/* <div className="header">
-          <Avatar onClick={this.reset}>
-            <Headline onClickLetsGo={this.setCollapsed} />
-          </Avatar>
-          <Profile />
-        </div> */}
-        <CoverPage />
+        <CoverPage
+          setCollapsed={this.setCollapsed}
+          reset={this.reset}
+        />
         {this.state.collapsed ? null : <StoryPanel />}
       </div>
     );
   }
 }
-
-const CoverPage = () => {
-  return (
-    <div className="cover-page">
-      <Avatar onClick={this.reset} />
-      <Headline onClickLetsGo={this.setCollapsed} />
-      <div className="name-and-bullets">
-        <h2>Morgan Keys</h2>
-        <div>
-          <a
-            className="subtle"
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://twitter.com/@morgankeys"
-          >
-            @morgankeys
-          </a>
-        </div>
-        <div className="bullet-points">
-          {BulletPoints.map((bullet, index) => {
-            return <p key={index}>{bullet}</p>;
-          })}
-        </div>
-      </div>
-    </div>
-  );
-};
 
 export default App;
