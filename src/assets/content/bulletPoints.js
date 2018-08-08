74560b2e2a6f983f77dc3be031127cd09003115d
Morgan Keys <morgan.keys@gmail.com>
2018-08-07 18:20:45 -0700

    Reworked some components

--- src/assets/content/bulletPoints.js
+++ src/assets/content/bulletPoints.js
@@ -1,5 +1,5 @@
 const bulletPoints = [
-  "Currently at BuildingConnected. Previously OpenGov, Hightail",
+  "Currently at BuildingConnected. Previously OpenGov, Hightail.",
   "Bachelor’s in Math and Creative Writing",
   "Master’s in Human-Computer Interaction, Decision Science",
   "Focus on enterprise workflow products",
