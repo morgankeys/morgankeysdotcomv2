e776be805f3373b825ff639fd5886d1fdc8b35b5
morgan <morgan.keys@gmail.com>
2019-07-08 22:35:41 -0700

    Tweak style system

--- src/styles/design-system.js
+++ src/styles/design-system.js
@@ -1,8 +1,34 @@
 module.exports = {
     colors: {
-        red: "#cf3232"
+        red: "#cf3232",
+        blue: "#0088ff",
     },
     gray: {
-        90: "#181818"
-    } 
+        90: "#181818",
+        40: "#757575",
+    },
+    fonts: {
+        display: "'Markazi Text', 'Constantia', serif", //400,500,600,700
+        body: "'Lato', 'Raleway', sans-serif", //100,300,400,700,900
+    },
+    breakpoints: {
+        SM: "24em",
+        MD: "36em",
+        LG: "48em",
+        XL: "60em",
+        mobile: "425px",
+        tablet: "768px",
+    },
+    spacer: (multiplier) => {
+        return `${multiplier*8}px`
+    },
+    zindex: (type, step) => {
+        let typeMap = {
+            overlay: 1000,
+            modal: 800,
+            content: 100,
+            background: -100
+        }
+        return typeMap[type]? (typeMap[type] + step) : (step)
+    },
 }
