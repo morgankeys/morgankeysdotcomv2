35234046053cd7015324a8dacca656232be4f6da
morgan <morgan.keys@gmail.com>
2019-08-08 23:22:01 -0700

    Build out coa case-study skeleton with images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import "./styles/App.css";
 import Home from "./components/Home/Home";
+import CaseStudies from "./components/CaseStudies";
 
 class App extends Component {
   render() {
-    return <Home />;
+    return <Router>
+        <Route path="/" exact component={Home} />
+        <Route path="/case-studies" exact component={CaseStudies} />
+      </Router>;
   }
 }
 
 export default App;
+
