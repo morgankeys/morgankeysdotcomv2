a125b2a0077c0b5dbd669f16d5188bf5e06c44fb
morgan <morgan.keys@gmail.com>
2019-08-13 23:08:10 -0700

    Kill useless css ref

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route} from "react-router-dom";
 
-import "./styles/App.css";
 import Home from "./components/Home/Home";
 import CaseStudiesIndex from "./components/CaseStudies/";
 import BCDS from "./components/CaseStudies/bcds";
