83aa3a7b981029afdd6ed3412507d19a7053627c
morgan <morgan.keys@gmail.com>
2019-09-07 10:26:33 -0700

    Add redirect-to-welcome for bad URLs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 
 import Welcome from "./sections/Welcome";
 import Home from "./sections/Home";
@@ -13,14 +13,17 @@ import "./styles/system.scss"
 class App extends Component {
   render() {
     return (
-        <Router>
+      <Router>
+        <Switch>
           <Route path="/" exact component={Welcome} />
           <Route path="/case-studies" exact component={CaseStudiesIndex} />
           <Route path="/home" exact component={Home} />
           <Route path="/bc-design-systems" exact component={BCDS} />
           <Route path="/bc-client-suggestions" exact component={BCClientSuggestions} />
           <Route path="/og-coa" exact component={OGCoa} />
-        </Router>
+          <Route component={Welcome} />
+        </Switch>
+      </Router>
     );
   }
 }
