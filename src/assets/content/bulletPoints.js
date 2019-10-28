e6590df33e461d57b212fc34dd049afd8384da44
morgan <morgan.keys@gmail.com>
2019-10-28 08:48:46 -0700

    Comma

--- src/assets/content/bulletPoints.js
+++ src/assets/content/bulletPoints.js
@@ -2,7 +2,7 @@ const bulletPoints = [
   "Currently at BuildingConnected\n (an Autodesk company)", 
   "Previously OpenGov, Hightail",
   "Master’s in Human-Computer Interaction, Decision Science",
-  "Bachelor’s in Math and Creative Writing",
+  "Bachelor’s in Math, Creative Writing",
   "Interest in prototyping and design systems",
   "Interest in applying decision science to design better collaboration and communities",
   "Technologist with React experience"
